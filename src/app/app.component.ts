97a0e6fee0dfef7d8f2532f305ab2deed3af7c15
nadun96 <nadunudaraka@gmail.com>
2025-06-29 00:59:07 +0530

    material ui added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopbarComponent } from "./components/topbar/topbar.component";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopbarComponent],
+  imports: [RouterOutlet, TopbarComponent,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
