c41798de6e1435d60cf41235932aa961d096702e
nadun96 <nadunudaraka@gmail.com>
2025-06-30 18:44:58 +0530

    create account ui added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { Routes } from '@angular/router';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { NgModule } from '@angular/core';
 import { LoginComponent} from './components/pages/login/login.component'
+import { CreateAccountComponent } from './components/pages/create-account/create-account.component';
 
 export const routes: Routes = [
-    {'path': 'login', component: LoginComponent},
+    { 'path': 'login', component: LoginComponent },
+    { 'path': 'signup', component: CreateAccountComponent},
 ];
--- src/app/components/pages/create-account/create-account.component.ts
+++ src/app/components/pages/create-account/create-account.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-account',
-  imports: [],
+  imports: [MatButtonModule, MatInputModule,MatLabel,MatFormFieldModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss'
 })
